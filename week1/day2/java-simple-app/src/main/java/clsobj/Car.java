bd3c1fe3ec9a99a1ef5dbad78b3c489843259ad5
akhila.g <akhila.g@collabera.com>
2022-01-09 14:18:49 +0530

    Car file

---
+++ week1/day2/java-simple-app/src/main/java/clsobj/Car.java
@@ -0,0 +1,16 @@
+package clsobj;
+
+public class Car {
+    public int speed;
+
+    {
+
+        speed = 10;
+
+
+        //parameterized
+    public Car( int sp){
+        speed = sp;
+    }
+    }
+}

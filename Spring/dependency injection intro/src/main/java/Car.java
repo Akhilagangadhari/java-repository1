b9820707daf91bac28c4329af17baba6de19fbba
akhila.g <akhila.g@collabera.com>
2022-01-11 23:02:20 +0530

    spring:dependency injection intro

---
+++ Spring/dependency injection intro/src/main/java/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+        private String model;
+        private int speed;
+
+        public String getModel() {
+            return model;
+            public int getSpeed() {
+                return speed;
+            }
+
+            public void setModel(String model) {
+                this.model = model;
+                public void setSpeed(int speed) {
+                    this.speed = speed;
+                }
+            }

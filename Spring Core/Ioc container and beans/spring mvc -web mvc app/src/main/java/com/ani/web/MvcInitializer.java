b7b7cc93ce39d7850099395faee38bb40b1d1304
akhila.g <akhila.g@collabera.com>
2022-01-12 00:34:18 +0530

    spring mvc : ani practice app

---
+++ Spring Core/Ioc container and beans/spring mvc -web mvc app/src/main/java/com/ani/web/MvcInitializer.java
@@ -0,0 +1,5 @@
+package com.ani.web;
+import com.ani.config.MvcConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+public class MvcInitializer {
+}
